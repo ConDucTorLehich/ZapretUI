5aff5592b7c08d6b5fed1454ea94aff0cb9f21b1
ConDucTorLehich <fedkova2001@gmail.com>
2025-07-29 17:21:01 +0700

    -Fixes

--- ZapretUI/Properties/AssemblyInfo.cs
+++ ZapretUI/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
 [assembly: AssemblyTitle("ZapretUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Application for personal use with autoUpdate")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ConDucTor")]
 [assembly: AssemblyProduct("ZapretUI")]
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
 // COM, следует установить атрибут ComVisible в TRUE для этого типа.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("3b9f4f5a-c9c4-49c6-9734-3bff33b1b282")]
